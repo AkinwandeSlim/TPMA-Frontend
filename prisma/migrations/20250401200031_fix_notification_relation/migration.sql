a9898427fdae7ec9b45a7ade36efc1a8db98b427
AkinwandeSlim <akinwandealex95@gmail.com>
2025-05-05 03:41:50 +0100

    Remove prisma folder and dependencies; update formValidationSchemas.ts and ESLint config

--- prisma/migrations/20250401200031_fix_notification_relation/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "Notification" ADD CONSTRAINT "Notification_teacherTraineeId_fkey" FOREIGN KEY ("teacherTraineeId") REFERENCES "TeacherTrainee"("id") ON DELETE SET NULL ON UPDATE CASCADE;
